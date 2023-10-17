acc1a99561063271152494c6d77620f33f997334
hongduhyeon <ghdengus12@naver.com>
2023-10-17 23:30:59 +0900

    Initial Commit

---
+++ SympleWeatherPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SympleWeatherPractice
+//
+//  Created by 홍두현 on 10/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SympleWeatherPracticeUITests/SympleWeatherPracticeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SympleWeatherPracticeUITestsLaunchTests.swift
+//  SympleWeatherPracticeUITests
+//
+//  Created by 홍두현 on 10/17/23.
+//
+
+import XCTest
+
+final class SympleWeatherPracticeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
